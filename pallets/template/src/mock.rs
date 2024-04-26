3aa21ff2f6bb6ed5969c8e7baa15098f97745744
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-04-26 01:33:58 -0500

    change: add template pallet

---
+++ pallets/template/src/mock.rs
@@ -0,0 +1,75 @@
+//! Test environment for template pallet.
+
+use crate::{self as pallet_template, Config};
+use frame_support::traits::{ConstU16, ConstU32, ConstU64};
+use sp_core::H256;
+use sp_io::TestExternalities;
+use sp_runtime::traits::{BlakeTwo256, IdentityLookup};
+
+pub type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+    pub enum Test
+    {
+        System: frame_system,
+        Balances: pallet_balances,
+        Template: pallet_template,
+    }
+);
+
+impl frame_system::Config for Test {
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Nonce = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = ConstU64<250>;
+    type DbWeight = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<u64>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ConstU16<42>;
+    type OnSetCode = ();
+    type MaxConsumers = ConstU32<16>;
+}
+
+impl pallet_balances::Config for Test {
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type MaxLocks = ConstU32<10>;
+    type Balance = u64;
+    type RuntimeEvent = RuntimeEvent;
+    type DustRemoval = ();
+    type ExistentialDeposit = ConstU64<1>;
+    type AccountStore = System;
+    type WeightInfo = ();
+    type FreezeIdentifier = ();
+    type MaxFreezes = ();
+    type RuntimeHoldReason = ();
+    type RuntimeFreezeReason = ();
+    type MaxHolds = ();
+}
+
+impl Config for Test {
+    type WeightInfo = ();
+    type RuntimeEvent = RuntimeEvent;
+}
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    let mut ext = TestExternalities::new(Default::default());
+    ext.execute_with(|| {
+        System::set_block_number(1);
+    });
+    ext
+}
