0cfeded9210ef7c716e2f781b0c1bd69dc76987b
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-08-30 16:57:27 -0500

    change(fc-pallet-gas-burn): handle inner signed extension logic

--- pallets/gas-burn/src/extensions.rs
+++ pallets/gas-burn/src/extensions.rs
@@ -2,75 +2,108 @@ use codec::{Decode, Encode};
 use core::marker::PhantomData;
 use frame_support::{
     dispatch::{DispatchInfo, Pays, PostDispatchInfo},
-    pallet_prelude::InvalidTransaction,
-    weights::Weight,
-    DebugNoBound,
+    pallet_prelude::{TransactionValidityError, ValidTransaction},
 };
-use frame_system::pallet_prelude::RuntimeCallFor;
 use scale_info::{StaticTypeInfo, TypeInfo};
 use sp_runtime::traits::{DispatchInfoOf, Dispatchable, SignedExtension};
 
 use crate::{traits::GasBurner, AccountIdOf, Config, Event, Pallet};
 
-#[derive(Encode, Decode, Clone, Eq, PartialEq, DebugNoBound)]
-pub struct ChargeTxWithPrepaidGas<T, S>(PhantomData<(T, S)>);
+#[derive(Encode, Decode, Clone, Eq, PartialEq)]
+pub struct ChargeTxBurningGas<T, S>(pub S, PhantomData<T>);
 
 // Make this extension "invisible" from the outside (ie metadata type information)
-impl<T, S: StaticTypeInfo> TypeInfo for ChargeTxWithPrepaidGas<T, S> {
+impl<T, S: StaticTypeInfo> TypeInfo for ChargeTxBurningGas<T, S> {
     type Identity = S;
     fn type_info() -> scale_info::Type {
         S::type_info()
     }
 }
 
-impl<T, S> SignedExtension for ChargeTxWithPrepaidGas<T, S>
+impl<T, S: Encode> core::fmt::Debug for ChargeTxBurningGas<T, S> {
+    #[cfg(feature = "std")]
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "ChargeTxBurningGas<{:?}>", self.0.encode())
+    }
+    #[cfg(not(feature = "std"))]
+    fn fmt(&self, _: &mut core::fmt::Formatter) -> core::fmt::Result {
+        Ok(())
+    }
+}
+
+impl<T, S> ChargeTxBurningGas<T, S> {
+    pub fn new(s: S) -> Self {
+        Self(s, PhantomData)
+    }
+}
+
+impl<T, S> SignedExtension for ChargeTxBurningGas<T, S>
 where
     T: Config + Send + Sync,
     T::RuntimeCall: Dispatchable<Info = DispatchInfo, PostInfo = PostDispatchInfo>,
     S: SignedExtension<AccountId = T::AccountId>,
+    S::Call: Dispatchable<Info = DispatchInfo, PostInfo = PostDispatchInfo>,
 {
-    const IDENTIFIER: &'static str = "CheckGasHandler";
+    const IDENTIFIER: &'static str = S::IDENTIFIER;
     type AccountId = AccountIdOf<T>;
-    type Call = RuntimeCallFor<T>;
-    type AdditionalSigned = ();
-    type Pre = AccountIdOf<T>;
+    type Call = S::Call;
+    type AdditionalSigned = S::AdditionalSigned;
+    type Pre = (AccountIdOf<T>, Option<S::Pre>);
 
-    fn additional_signed(
-        &self,
-    ) -> Result<Self::AdditionalSigned, frame_support::pallet_prelude::TransactionValidityError>
-    {
-        Ok(())
+    fn additional_signed(&self) -> Result<Self::AdditionalSigned, TransactionValidityError> {
+        self.0.additional_signed()
     }
 
     fn pre_dispatch(
         self,
         who: &Self::AccountId,
-        _call: &RuntimeCallFor<T>,
-        info: &DispatchInfoOf<RuntimeCallFor<T>>,
-        _len: usize,
-    ) -> Result<Self::Pre, frame_support::pallet_prelude::TransactionValidityError> {
-        T::GasHandler::check_available_gas(who, &Some(info.weight))
-            .map(|_| who.clone())
-            .ok_or(InvalidTransaction::Payment.into())
+        call: &Self::Call,
+        info: &DispatchInfoOf<Self::Call>,
+        len: usize,
+    ) -> Result<Self::Pre, TransactionValidityError> {
+        let pre = if let Some(_) = T::GasHandler::check_available_gas(who, &Some(info.weight)) {
+            None
+        } else {
+            Some(self.0.pre_dispatch(who, call, info, len)?)
+        };
+
+        Ok((who.clone(), pre))
+    }
+
+    fn validate(
+        &self,
+        who: &Self::AccountId,
+        call: &Self::Call,
+        info: &DispatchInfoOf<Self::Call>,
+        len: usize,
+    ) -> frame_support::pallet_prelude::TransactionValidity {
+        if let Some(_) = T::GasHandler::check_available_gas(who, &Some(info.weight)) {
+            Ok(ValidTransaction::default())
+        } else {
+            self.0.validate(who, call, info, len)
+        }
     }
 
     fn post_dispatch(
         pre: Option<Self::Pre>,
         info: &DispatchInfoOf<Self::Call>,
         post_info: &sp_runtime::traits::PostDispatchInfoOf<Self::Call>,
-        _len: usize,
-        _result: &sp_runtime::DispatchResult,
+        len: usize,
+        result: &sp_runtime::DispatchResult,
     ) -> Result<(), frame_support::pallet_prelude::TransactionValidityError> {
-        let who = pre.expect("pre given on pre_dispatch; qed");
+        if let Some((who, maybe_inner_pre)) = pre {
+            if let Some(pre) = maybe_inner_pre {
+                S::post_dispatch(Some(pre), &info.clone().into(), post_info, len, result)?;
+            } else {
+                let actual_weight = post_info.actual_weight.unwrap_or(info.weight);
+                let should_burn_gas = post_info.pays_fee == Pays::Yes;
 
-        let actual_weight = post_info.actual_weight.unwrap_or(info.weight);
-        let should_burn_gas = post_info.pays_fee == Pays::Yes;
-
-        if should_burn_gas {
-            let remaining = T::GasHandler::burn_gas(&who, &actual_weight);
-            Pallet::<T>::deposit_event(Event::GasBurned { who, remaining });
+                if should_burn_gas {
+                    let remaining = T::GasHandler::burn_gas(&who, &actual_weight);
+                    Pallet::<T>::deposit_event(Event::GasBurned { who, remaining });
+                }
+            }
         }
-
         Ok(())
     }
 }
