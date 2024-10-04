52e1fb13108912821e6e6dc989dd37c140fd145e
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-10-03 20:30:25 -0500

    fix(fc-traits-gas-tank): missing set/clear attributes

--- traits/gas-tank/src/impl_nonfungibles.rs
+++ traits/gas-tank/src/impl_nonfungibles.rs
@@ -83,34 +83,27 @@ where
     }
 
     fn burn_gas(who: &Self::AccountId, expected: &Self::Gas, used: &Self::Gas) -> Self::Gas {
-        let Some(mut gas_tank): Option<MembershipWeightTank<T>> =
-            F::owned(who).find_map(|(collection, item)| {
-                let expected_remaining: Weight = F::typed_system_attribute(
+        F::owned(who)
+            .find_map(|(collection, item)| {
+                if !expected.eq(&F::typed_system_attribute(
                     &collection,
                     Some(&item),
                     &ATTR_GAS_TX_PAY_WITH_MEMBERSHIP,
-                )?;
-
-                if expected.eq(&expected_remaining) {
-                    Some(F::typed_system_attribute(
-                        &collection,
-                        Some(&item),
-                        &ATTR_MEMBERSHIP_GAS,
-                    )?)
-                } else {
-                    None
+                )?) {
+                    return None;
                 }
+                F::clear_typed_attribute(&collection, &item, &ATTR_GAS_TX_PAY_WITH_MEMBERSHIP)
+                    .ok()?;
+
+                let mut gas_tank: MembershipWeightTank<T> =
+                    F::typed_system_attribute(&collection, Some(&item), &ATTR_MEMBERSHIP_GAS)?;
+
+                gas_tank.used = gas_tank.used.checked_add(used)?;
+
+                F::set_typed_attribute(&collection, &item, &ATTR_MEMBERSHIP_GAS, &gas_tank).ok()?;
+                let max_weight = gas_tank.max_per_period?;
+                Some(max_weight.saturating_sub(gas_tank.used))
             })
-        else {
-            return Weight::zero();
-        };
-        let Some(max_weight) = gas_tank.max_per_period else {
-            return Weight::MAX;
-        };
-
-        gas_tank.used = gas_tank.used.add_proof_size(used.proof_size());
-        gas_tank.used = gas_tank.used.add_ref_time(used.ref_time());
-
-        max_weight.saturating_sub(gas_tank.used)
+            .unwrap_or_default()
     }
 }
