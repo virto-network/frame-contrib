9d27230d43c5641302f6e06e95960a418ec96ff9
Lex Luthor <johan@nulo.network>
2025-10-08 16:23:20 +0200

    Migration to stable 2509, pending to fix 2 test: session_duration_is_met and max_sessions_per_account_works

--- pallets/referenda-tracks/src/lib.rs
+++ pallets/referenda-tracks/src/lib.rs
@@ -29,7 +29,7 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-use alloc::{borrow::Cow, vec::Vec};
+use alloc::borrow::Cow;
 use frame_support::traits::OriginTrait;
 use pallet_referenda::{BalanceOf, BlockNumberFor, PalletsOriginOf, Track, TrackInfoOf};
 use sp_core::Get;
