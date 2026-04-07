31b3815d0068f4f3b4760874cf278c4dc0000c52
Daniel Olano <me@olanod.com>
2026-04-07 10:29:29 +0200

    traits-authn: Gate frame-support behind runtime feature
    Make frame-support optional. Core type aliases (Challenge, DeviceId,
    AuthorityId, HashedUserId) and ExtrinsicContext stay unconditional so
    client crates can depend on traits-authn without pulling in the entire
    Substrate runtime stack. The traits that need Get/Parameter (Challenger,
    Authenticator, UserAuthenticator, etc.) and the util module (Auth, Dev,
    AuthorityFromPalletId) are gated behind the new runtime feature.
    
    Downstream pallet-pass and frame-contrib-traits explicitly enable
    traits-authn/runtime since they're always runtime-side consumers.

--- traits/authn/src/util.rs
+++ traits/authn/src/util.rs
@@ -1,223 +1,236 @@
-use core::marker::PhantomData;
-
-use codec::{Decode, DecodeWithMemTracking, Encode, FullCodec, MaxEncodedLen};
-use frame_support::{sp_runtime::traits::TrailingZeroInput, traits::Get, PalletId};
-use scale_info::TypeInfo;
-
-use crate::{
-    Authenticator, AuthorityId, Challenger, CxOf, DeviceChallengeResponse, DeviceId,
-    UserAuthenticator, UserChallengeResponse,
-};
-
-type ChallengerOf<Dev> = <Dev as UserAuthenticator>::Challenger;
-
-#[derive(Encode, Decode, DecodeWithMemTracking, TypeInfo, Clone, PartialEq, Eq, Debug)]
-#[scale_info(skip_type_params(Id))]
-pub struct AuthorityFromPalletId<Id>(PhantomData<Id>);
-
-impl<Id: Get<PalletId>> Get<AuthorityId> for AuthorityFromPalletId<Id> {
-    fn get() -> AuthorityId {
-        Decode::decode(&mut TrailingZeroInput::new(&Id::get().0))
-            .expect("Size of PalletId is less than 32 bytes; qed")
-    }
-}
-
-#[derive(Encode, Decode, DecodeWithMemTracking, TypeInfo, Clone, PartialEq, Eq, Debug)]
-#[scale_info(skip_type_params(Dev, Att))]
-/// Convenient auto-implementor of the Authenticator trait
-pub struct Auth<Dev, Att>(PhantomData<(Dev, Att)>);
-
-impl<Dev, Att> Authenticator for Auth<Dev, Att>
-where
-    Att: DeviceChallengeResponse<CxOf<ChallengerOf<Dev>>>,
-    Dev: UserAuthenticator + From<Att>,
-{
-    type Authority = <Dev as UserAuthenticator>::Authority;
-    type Challenger = ChallengerOf<Dev>;
-    type DeviceAttestation = Att;
-    type Device = Dev;
-
-    fn unpack_device(attestation: Self::DeviceAttestation) -> Self::Device {
-        attestation.into()
-    }
-}
-
 pub trait VerifyCredential<Cred> {
     fn verify(&mut self, credential: &Cred) -> Option<()>;
 }
 
-/// Convenient auto-implementor of the UserAuthenticator trait
-#[derive(Encode, Decode, DecodeWithMemTracking, TypeInfo, Clone, PartialEq, Eq, Debug)]
-#[scale_info(skip_type_params(A, Ch, Cred))]
-pub struct Dev<T, A, Ch, Cred>(T, PhantomData<(A, Ch, Cred)>);
+#[cfg(feature = "runtime")]
+mod runtime {
+    use core::marker::PhantomData;
 
-impl<T, A, Ch, Cred> Dev<T, A, Ch, Cred> {
-    pub fn new(t: T) -> Self {
-        Self(t, PhantomData)
-    }
-}
-
-impl<T, A, Ch, Cred> UserAuthenticator for Dev<T, A, Ch, Cred>
-where
-    T: VerifyCredential<Cred> + AsRef<DeviceId> + FullCodec + MaxEncodedLen + TypeInfo + 'static,
-    A: Get<AuthorityId> + 'static,
-    Ch: Challenger + 'static,
-    Cred: UserChallengeResponse<Ch::Context> + 'static + Send + Sync,
-{
-    type Authority = A;
-    type Challenger = Ch;
-    type Credential = Cred;
-
-    fn verify_credential(&mut self, credential: &Self::Credential) -> Option<()> {
-        self.0.verify(credential)
-    }
-
-    fn device_id(&self) -> &DeviceId {
-        self.0.as_ref()
-    }
-}
-
-impl<T: MaxEncodedLen, A, Ch, Cr> MaxEncodedLen for Dev<T, A, Ch, Cr> {
-    fn max_encoded_len() -> usize {
-        T::max_encoded_len()
-    }
-}
-
-pub mod dummy {
-    use super::*;
-    use frame_support::{
-        parameter_types, sp_runtime::str_array as s, CloneNoBound, DebugNoBound, EqNoBound,
-        PartialEqNoBound,
-    };
+    use codec::{Decode, DecodeWithMemTracking, Encode, FullCodec, MaxEncodedLen};
+    use frame_support::{sp_runtime::traits::TrailingZeroInput, traits::Get, PalletId};
     use scale_info::TypeInfo;
 
     use crate::{
-        AuthorityId, Challenger, DeviceChallengeResponse, DeviceId, ExtrinsicContext, HashedUserId,
-        UserChallengeResponse,
+        Authenticator, AuthorityId, Challenger, CxOf, DeviceChallengeResponse, DeviceId,
+        UserAuthenticator, UserChallengeResponse,
     };
 
-    use super::{Auth, Dev, VerifyCredential};
+    use super::VerifyCredential;
 
-    #[derive(
-        Default,
-        CloneNoBound,
-        DebugNoBound,
-        Encode,
-        Decode,
-        DecodeWithMemTracking,
-        MaxEncodedLen,
-        TypeInfo,
-        PartialEqNoBound,
-        EqNoBound,
-    )]
-    #[scale_info(skip_type_params(A))]
-    pub struct DummyAttestation<A>(bool, Option<DeviceId>, PhantomData<A>);
+    type ChallengerOf<Dev> = <Dev as UserAuthenticator>::Challenger;
 
-    impl<A> DummyAttestation<A> {
-        pub fn new(value: bool, device_id: DeviceId) -> Self {
-            Self(value, Some(device_id), PhantomData)
+    #[derive(Encode, Decode, DecodeWithMemTracking, TypeInfo, Clone, PartialEq, Eq, Debug)]
+    #[scale_info(skip_type_params(Id))]
+    pub struct AuthorityFromPalletId<Id>(PhantomData<Id>);
+
+    impl<Id: Get<PalletId>> Get<AuthorityId> for AuthorityFromPalletId<Id> {
+        fn get() -> AuthorityId {
+            Decode::decode(&mut TrailingZeroInput::new(&Id::get().0))
+                .expect("Size of PalletId is less than 32 bytes; qed")
         }
     }
 
-    #[derive(
-        CloneNoBound,
-        DebugNoBound,
-        Encode,
-        Decode,
-        DecodeWithMemTracking,
-        MaxEncodedLen,
-        TypeInfo,
-        PartialEqNoBound,
-        EqNoBound,
-    )]
-    #[scale_info(skip_type_params(A))]
-    pub struct DummyCredential<A>(bool, HashedUserId, PhantomData<A>);
+    #[derive(Encode, Decode, DecodeWithMemTracking, TypeInfo, Clone, PartialEq, Eq, Debug)]
+    #[scale_info(skip_type_params(Dev, Att))]
+    /// Convenient auto-implementor of the Authenticator trait
+    pub struct Auth<Dev, Att>(PhantomData<(Dev, Att)>);
 
-    impl<A> DummyCredential<A> {
-        pub fn new(value: bool, user_id: HashedUserId) -> Self {
-            Self(value, user_id, PhantomData)
-        }
-    }
-
-    type DummyChallenger = u8;
-    type DummyCx = <DummyChallenger as Challenger>::Context;
-
-    parameter_types! {
-        const DummyAuthority: AuthorityId = s("dummy_authority");
-    }
-    pub const DUMMY_DEV: DeviceId = s("dummy_device");
-    pub const DUMMY_USER: HashedUserId = s("dummy_user_hash");
-
-    impl Challenger for DummyChallenger {
-        type Context = Self;
-        fn generate(cx: &Self::Context, _xtc: &impl ExtrinsicContext) -> crate::Challenge {
-            [*cx; 32]
-        }
-    }
-
-    pub type DummyDev<AuthorityId> = Dev<
-        DummyAttestation<AuthorityId>,
-        AuthorityId,
-        DummyChallenger,
-        DummyCredential<AuthorityId>,
-    >;
-    pub type Dummy<AuthorityId> = Auth<DummyDev<AuthorityId>, DummyAttestation<AuthorityId>>;
-
-    impl<A> From<DummyAttestation<A>> for DummyDev<A> {
-        fn from(value: DummyAttestation<A>) -> Self {
-            DummyDev::new(value)
-        }
-    }
-
-    impl<A> AsRef<DeviceId> for DummyAttestation<A> {
-        fn as_ref(&self) -> &DeviceId {
-            self.1.as_ref().unwrap_or(&DUMMY_DEV)
-        }
-    }
-
-    impl<A> VerifyCredential<DummyCredential<A>> for DummyAttestation<A> {
-        fn verify(&mut self, _: &DummyCredential<A>) -> Option<()> {
-            self.0.then_some(())
-        }
-    }
-
-    impl<A> DeviceChallengeResponse<DummyCx> for DummyAttestation<A>
+    impl<Dev, Att> Authenticator for Auth<Dev, Att>
     where
-        A: Get<AuthorityId> + 'static,
+        Att: DeviceChallengeResponse<CxOf<ChallengerOf<Dev>>>,
+        Dev: UserAuthenticator + From<Att>,
     {
-        fn is_valid(&self) -> bool {
-            self.0
+        type Authority = <Dev as UserAuthenticator>::Authority;
+        type Challenger = ChallengerOf<Dev>;
+        type DeviceAttestation = Att;
+        type Device = Dev;
+
+        fn unpack_device(attestation: Self::DeviceAttestation) -> Self::Device {
+            attestation.into()
+        }
+    }
+
+    /// Convenient auto-implementor of the UserAuthenticator trait
+    #[derive(Encode, Decode, DecodeWithMemTracking, TypeInfo, Clone, PartialEq, Eq, Debug)]
+    #[scale_info(skip_type_params(A, Ch, Cred))]
+    pub struct Dev<T, A, Ch, Cred>(T, PhantomData<(A, Ch, Cred)>);
+
+    impl<T, A, Ch, Cred> Dev<T, A, Ch, Cred> {
+        pub fn new(t: T) -> Self {
+            Self(t, PhantomData)
+        }
+    }
+
+    impl<T, A, Ch, Cred> UserAuthenticator for Dev<T, A, Ch, Cred>
+    where
+        T: VerifyCredential<Cred>
+            + AsRef<DeviceId>
+            + FullCodec
+            + MaxEncodedLen
+            + TypeInfo
+            + 'static,
+        A: Get<AuthorityId> + 'static,
+        Ch: Challenger + 'static,
+        Cred: UserChallengeResponse<Ch::Context> + 'static + Send + Sync,
+    {
+        type Authority = A;
+        type Challenger = Ch;
+        type Credential = Cred;
+
+        fn verify_credential(&mut self, credential: &Self::Credential) -> Option<()> {
+            self.0.verify(credential)
         }
 
-        fn used_challenge(&self) -> (DummyCx, crate::Challenge) {
-            (0, [0; 32])
-        }
-        fn authority(&self) -> AuthorityId {
-            A::get()
-        }
         fn device_id(&self) -> &DeviceId {
-            self.1.as_ref().unwrap_or(&DUMMY_DEV)
+            self.0.as_ref()
         }
     }
 
-    impl<A> UserChallengeResponse<DummyCx> for DummyCredential<A>
-    where
-        A: Get<AuthorityId> + 'static,
-    {
-        fn is_valid(&self) -> bool {
-            self.0
+    impl<T: MaxEncodedLen, A, Ch, Cr> MaxEncodedLen for Dev<T, A, Ch, Cr> {
+        fn max_encoded_len() -> usize {
+            T::max_encoded_len()
+        }
+    }
+
+    pub mod dummy {
+        use super::*;
+        use frame_support::{
+            parameter_types, sp_runtime::str_array as s, CloneNoBound, DebugNoBound, EqNoBound,
+            PartialEqNoBound,
+        };
+        use scale_info::TypeInfo;
+
+        use crate::{
+            AuthorityId, Challenger, DeviceChallengeResponse, DeviceId, ExtrinsicContext,
+            HashedUserId, UserChallengeResponse,
+        };
+
+        use super::{Auth, Dev, VerifyCredential};
+
+        #[derive(
+            Default,
+            CloneNoBound,
+            DebugNoBound,
+            Encode,
+            Decode,
+            DecodeWithMemTracking,
+            MaxEncodedLen,
+            TypeInfo,
+            PartialEqNoBound,
+            EqNoBound,
+        )]
+        #[scale_info(skip_type_params(A))]
+        pub struct DummyAttestation<A>(bool, Option<DeviceId>, PhantomData<A>);
+
+        impl<A> DummyAttestation<A> {
+            pub fn new(value: bool, device_id: DeviceId) -> Self {
+                Self(value, Some(device_id), PhantomData)
+            }
         }
 
-        fn used_challenge(&self) -> (DummyCx, crate::Challenge) {
-            (0, [0; 32])
+        #[derive(
+            CloneNoBound,
+            DebugNoBound,
+            Encode,
+            Decode,
+            DecodeWithMemTracking,
+            MaxEncodedLen,
+            TypeInfo,
+            PartialEqNoBound,
+            EqNoBound,
+        )]
+        #[scale_info(skip_type_params(A))]
+        pub struct DummyCredential<A>(bool, HashedUserId, PhantomData<A>);
+
+        impl<A> DummyCredential<A> {
+            pub fn new(value: bool, user_id: HashedUserId) -> Self {
+                Self(value, user_id, PhantomData)
+            }
         }
 
-        fn authority(&self) -> AuthorityId {
-            A::get()
+        type DummyChallenger = u8;
+        type DummyCx = <DummyChallenger as Challenger>::Context;
+
+        parameter_types! {
+            const DummyAuthority: AuthorityId = s("dummy_authority");
+        }
+        pub const DUMMY_DEV: DeviceId = s("dummy_device");
+        pub const DUMMY_USER: HashedUserId = s("dummy_user_hash");
+
+        impl Challenger for DummyChallenger {
+            type Context = Self;
+            fn generate(cx: &Self::Context, _xtc: &impl ExtrinsicContext) -> crate::Challenge {
+                [*cx; 32]
+            }
         }
 
-        fn user_id(&self) -> HashedUserId {
-            self.1
+        pub type DummyDev<AuthorityId> = Dev<
+            DummyAttestation<AuthorityId>,
+            AuthorityId,
+            DummyChallenger,
+            DummyCredential<AuthorityId>,
+        >;
+        pub type Dummy<AuthorityId> = Auth<DummyDev<AuthorityId>, DummyAttestation<AuthorityId>>;
+
+        impl<A> From<DummyAttestation<A>> for DummyDev<A> {
+            fn from(value: DummyAttestation<A>) -> Self {
+                DummyDev::new(value)
+            }
+        }
+
+        impl<A> AsRef<DeviceId> for DummyAttestation<A> {
+            fn as_ref(&self) -> &DeviceId {
+                self.1.as_ref().unwrap_or(&DUMMY_DEV)
+            }
+        }
+
+        impl<A> VerifyCredential<DummyCredential<A>> for DummyAttestation<A> {
+            fn verify(&mut self, _: &DummyCredential<A>) -> Option<()> {
+                self.0.then_some(())
+            }
+        }
+
+        impl<A> DeviceChallengeResponse<DummyCx> for DummyAttestation<A>
+        where
+            A: Get<AuthorityId> + 'static,
+        {
+            fn is_valid(&self) -> bool {
+                self.0
+            }
+
+            fn used_challenge(&self) -> (DummyCx, crate::Challenge) {
+                (0, [0; 32])
+            }
+            fn authority(&self) -> AuthorityId {
+                A::get()
+            }
+            fn device_id(&self) -> &DeviceId {
+                self.1.as_ref().unwrap_or(&DUMMY_DEV)
+            }
+        }
+
+        impl<A> UserChallengeResponse<DummyCx> for DummyCredential<A>
+        where
+            A: Get<AuthorityId> + 'static,
+        {
+            fn is_valid(&self) -> bool {
+                self.0
+            }
+
+            fn used_challenge(&self) -> (DummyCx, crate::Challenge) {
+                (0, [0; 32])
+            }
+
+            fn authority(&self) -> AuthorityId {
+                A::get()
+            }
+
+            fn user_id(&self) -> HashedUserId {
+                self.1
+            }
         }
     }
 }
+
+#[cfg(feature = "runtime")]
+pub use runtime::*;
